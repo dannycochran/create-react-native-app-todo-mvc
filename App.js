e1fcb0075e01d8954f552803784ce4cc0e2c023b
Presentation <Presentation@dannycochran-macbookpro.roam.corp.google.com>
2017-06-06 21:16:04 -0700

    almost there

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {
   View,
   ActivityIndicator,
-  StyleSheet,
   Text,
   ScrollView,
   AsyncStorage,
@@ -11,55 +10,91 @@ import {
   TouchableHighlight
 } from 'react-native';
 
+import styles, {
+  shadowProps,
+  buttonUnderlay
+} from './styles';
+
 import Todo from './Todo';
+import Footer from './Footer';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       todos: [],
+      selectedTab: 'active',
+      scrollEnabled: true,
       editing: false,
       loading: true,
       input: ''
     }
   }
 
+  async storeTodos(todos) {
+    await AsyncStorage.setItem('todos', JSON.stringify(todos));
+  }
+
   async addTodo() {
+    const timestamp = Date.now();
     const todos = [
       {
-        timestamp: Date.now(),
+        id: timestamp + this.state.input,
+        timestamp,
         completed: false,
         description: this.state.input
       },
       ...this.state.todos
     ];
 
-    await AsyncStorage.setItem('todos', JSON.stringify(todos));
+    await this.storeTodos(todos);
     this.setState({
       todos,
-      input: '',
-      editing: false
+      input: ''
     });
   }
 
-  fetchTodos() {
-    AsyncStorage.getItem('todos', (todos) => {
-      todos = JSON.parse(todos) || [];
-      this.setState(prevState => ({
-        ...prevState,
-        loading: false,
-        todos
-      }));
-    });
+  async fetchTodos() {
+    let todos = await AsyncStorage.getItem('todos');
+    todos = JSON.parse(todos) || [];
+    this.setState(prevState => ({
+      ...prevState,
+      loading: false,
+      todos
+    }));
+  }
+
+  onChangeTab(selectedTab) {
+    this.setState({ selectedTab });
+  }
+
+  onDragTodo(callback) {
+    this.setState({ scrollEnabled: false });
+  }
+
+  async onReleaseTodo(todoId, config) {
+    this.scrollView.scrollTo({ y: 0, animate: true });
+    const todoIndex = this.state.todos.findIndex(t => t.id === todoId);
+    const todos = [ ...this.state.todos ];
+
+    if (config.remove) {
+      todos.splice(todoIndex, 1);
+    } else if (config.complete) {
+      todos[todoIndex].completed = true;
+    }
+
+    await this.storeTodos(todos);
+    this.setState({ scrollEnabled: true, todos });
   }
 
   renderInput() {
     return (
       <View {...shadowProps} style={styles.inputContainer}>
-        <TextInput {...shadowProps} style={styles.textInput}
+        <TextInput {...shadowProps} style={this.state.input.length > 0 ? styles.textInputFocused : styles.textInput}
           onFocus={() => this.setState({ editing: true })}
           onBlur={() => this.setState({ editing: false })}
           placeholder={'What needs to be done?'}
+          value={this.state.input}
           onChangeText={(input) => this.setState({ input })} />
       </View>
     );
@@ -71,8 +106,10 @@ export default class App extends React.Component {
     }
 
     return (
-      <TouchableHighlight style={styles.addButton}>
-        <Text>Add Todo</Text>
+      <TouchableHighlight style={styles.addButton}
+        onPress={this.addTodo.bind(this)}
+        underlayColor={buttonUnderlay}>
+        <Text style={styles.addText}>Add Todo</Text>
       </TouchableHighlight>
     );
   }
@@ -85,74 +122,31 @@ export default class App extends React.Component {
     if (this.state.loading) {
       return <ActivityIndicator style={styles.centered} animating={true} size='large'/>;
     }
+    
+    const todoHandlers = {
+      onDragTodo: this.onDragTodo.bind(this),
+      onReleaseTodo: this.onReleaseTodo.bind(this)
+    };
+
+    const todos = this.state.todos.filter(todo => {
+      return this.state.selectedTab === 'active' ? todo.completed === false : todo.completed === true;
+    });
 
     return (
-      <KeyboardAvoidingView style={styles.appContainer} behavior='padding'>
-        <Text style={styles.headerText}>todos</Text>
-        <ScrollView>
+      <View style={styles.appContainer}>
+        <KeyboardAvoidingView style={styles.appWrapper} behavior='padding'>
+          <Text style={styles.headerText}>todos</Text>
+          {this.renderInput()}
           <View style={styles.scrollContainer}>
-            {this.renderInput()}
-            {this.state.todos.map(t => <Todo {...t} key={t.timestamp + t.description} />)}
+            <ScrollView ref={(ref) => this.scrollView = ref}
+              style={{flex: 1}} scrollEnabled={this.state.scrollEnabled}>
+              {todos.map(t => <Todo {...t} key={t.id} {...todoHandlers} />)}
+            </ScrollView>
           </View>
-        </ScrollView>
-        {this.renderAddButton()}
-      </KeyboardAvoidingView>
+          {this.renderAddButton()}
+        </KeyboardAvoidingView>
+        <Footer onChangeTab={this.onChangeTab.bind(this)} selectedTab={this.state.selectedTab} />
+      </View>
     );
   }
 }
-
-const shadowProps = {
-  shadowColor: 'black',
-  shadowOpacity: 0.2,
-  shadowOffset: {
-    height: 5
-  },
-  shadowRadius: 6
-};
-
-const styles = StyleSheet.create({
-  appContainer: {
-    flex: 1,
-    marginTop: 20
-  },
-
-  addButton: {
-    height: 60,
-    flexDirection: 'row',
-    backgroundColor: 'green'
-  },
-
-  scrollContainer: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  headerText: {
-    fontSize: 100,
-    fontWeight: '100',
-    color: 'rgba(175, 47, 47, 0.15)',
-    textAlign: 'center'
-  },
-
-  inputContainer: {
-    flex: 1,
-    height: 60,
-    elevation: 4,
-    flexDirection: 'row',
-    marginTop: 5
-  }, 
-
-  textInput: {
-    flex: 1,
-    textAlign: 'center',
-    fontStyle: 'italic',
-  },
-
-  centered: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
